3b9943ea8647565fe6c8198a2c0f9cd34c81782b
Elton <eltonwhybrow@icloud.com>
2023-12-23 15:18:20 +0000

    test emailimg

---
+++ wp-content/themes/widesign-2023/fp-horley.php
@@ -0,0 +1,321 @@
+<?php get_header();
+
+/*
+ * Template Name: Landing Page - Horley
+ * Template Post Type: post, page, press-release, people
+ */
+
+
+?>
+
+<div class="bg-white">
+	<!-- MESSAGE notification -->
+	<!-- <? //php echo do_shortcode("[get-message color='info' message='New tutoring service opening in the New Year']"); 
+			?> -->
+	<!-- HERO -->
+	<section class="flex flex-wrap md:flex-nowrap overflow-hidden">
+
+		<div class="relative pt-4 pb-6 md:pt-0 md:pb-0 w-full md:w-2/5 flex justify-center items-center bg-gray-900">
+			<div class="hex-container">
+
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+				<div class="row">
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+					<div class="hexagon"></div>
+				</div>
+
+			</div>
+			<div class="relative p-3 md:p-10 text-4xl lg:text-5xl lg:p-6">
+				<h2 class="pb-1 text-mellow-200 uppercase font-semibold">
+					Be bold
+				</h2>
+				<h2 class="pb-1 text-mellow-400 uppercase font-semibold">
+					Take chances
+				</h2>
+				<h2 class="pb-1 text-mellow-600 uppercase font-semibold">
+					Create beauty
+				</h2>
+				<div class="grid md:justify-items-end mt-2">
+					<a class="relative inline-flex pr-8 primary-btn  hover:bg-teal-500 bg-gray-800 group" href="#begin-journey">
+						Begin your journey
+						<svg xmlns="http://www.w3.org/2000/svg" class="absolute ml-1 top-3 right-1 inline-block transform group-hover:rotate-360 transition-rotate duration-500 w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="w-full md:w-3/5">
+			<div class="pb-1/3 lg:pb-4/5 xl:pb-2/3 2xl:pb-3/5  relative">
+				<img class="absolute top-0 left-0 h-full md:h-100 lg:h-full w-full object-cover" src="/resources/img/pages/home/widesign-elephant.jpg" alt="WideSign Web & SEO Services">
+			</div>
+		</div>
+	</section>
+
+	<!-- MAIN EDITOR CONTENT -->
+	<section id="begin-journey" class="py-20 px-4 md:px-10 bg-teal-700 text-center">
+		<?php if (have_posts()) : while (have_posts()) : the_post();
+				the_content();
+			endwhile;
+		else : ?>
+			<p>Sorry, no new content for now! 😞</p>
+		<?php endif; ?>
+	</section>
+
+	<!-- TECH STACK -->
+	<?php get_template_part('templates/partials/section', 'tech-stack');
+	?>
+
+	<!-- AUDIT -->
+	<?php (the_field('homepage_site_audit')) ?>
+
+	<!-- TESTIMONIALS -->
+	<!-- background-image:url('/resources/img/pages/home/testimonial-paralax.jpg'); -->
+	<section aria-label="testimonials" id="testinonials" class="w-full bg-gray-100 bg-testimonial" style="
+		background-repeat: no-repeat;
+		background-position: top left;
+		background-size: cover !important;
+		background-attachment: fixed !important;
+    ">
+		<div class="max-w-5xl mx-auto pt-16 pb-5 md:pb-20">
+			<div class="mx-8 flex flex-wrap md:flex-nowrap pt-5 md:pt-10">
+				<div class="py-0 w-full">
+					<?php echo do_shortcode('[customer-quotes number="10"]');
+					?>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+	<!-- FAQS -->
+	<?php get_template_part('templates/partials/section', 'faqs-shortcut');
+	?>
+
+	<!-- WHY US -->
+	<?php (the_field('homepage_whyus')) ?>
+
+	<!-- CALLBACK -->
+	<?php get_template_part('templates/partials/section', 'call-back');
+	?>
+
+</div>
+
+<?php get_footer(); ?>
